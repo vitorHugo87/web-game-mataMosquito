a23ae8523410a6c75ec9bf62c448fac3b5693711
vitorHugo87 <vitorpaduan87@gmail.com>
2024-10-21 17:16:54 -0300

    Criando Posições Randômicas

--- jogo.js
+++ jogo.js
@@ -8,5 +8,25 @@ function ajustaTamanhoPalcoJogo() {
     console.log(altura, largura);
 }
 
-ajustaTamanhoPalcoJogo()
+ajustaTamanhoPalcoJogo();
 
+
+function posicaoRandomica() {
+    var posicaoX = Math.floor(Math.random() * largura) - 90;
+    var posicaoY = Math.floor(Math.random() * altura) - 90;
+
+    posicaoX = (posicaoX < 0) ? 0 : posicaoX;
+    posicaoY = (posicaoY < 0) ? 0 : posicaoY;
+    
+    console.log(posicaoX, posicaoY);
+    
+    //Criar o elemento HTML
+    var mosca = document.createElement('img');
+    mosca.src = 'imagens/mosca.png';
+    mosca.className = 'mosquito1';
+    mosca.style.left = posicaoX + 'px';
+    mosca.style.top = posicaoY + 'px';
+    mosca.style.position = 'absolute';
+    
+    document.body.appendChild(mosca);
+}
