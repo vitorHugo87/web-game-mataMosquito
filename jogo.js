54ba59dcc19153d0ab219074705fa6bb67cdc08d
vitorHugo87 <vitorpaduan87@gmail.com>
2024-10-22 16:09:35 -0300

    Página de Vitória

--- jogo.js
+++ jogo.js
@@ -17,7 +17,7 @@ var cronometro = setInterval(function() {
     if (tempo < 0) {
         clearInterval(cronometro);
         clearInterval(criaMosca);
-        alert('Vitoria');
+        window.location.href = 'vitoria.html';
     } else {
         document.getElementById('cronometro').innerHTML = tempo;
     }
